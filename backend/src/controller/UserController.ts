29eebdfab5ab2e745e1f81876ebc1a0e825f6d70
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-07-29 13:42:53 -0300

    backend - user register controller creation

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -20,25 +20,68 @@ interface UserRegistrationRequest{
 };
 
 // user respose
-interface UserRegistrationResponse{
+interface UserResponse{
     id: string; name: string; email: string;
 };
 
 interface ApiResponse<T = any>{
-    success: boolean; message: string; data?: T; error?: string;
+    success: boolean; message: string; data?: T; errorMessage?: string;
 };
 
 
 // class
 class UserController{
 
+    // get error message
+    getErrorMessage(error: unknown): string {
+        if(error instanceof Error) return error.message;
+        return String(error);
+        // if "error" its a instance from Error, we can access his properties, like "error.message" with safety
+    };
+
     // register user
-    registerUser(
+    async registerUser(
         req: Request< {}, {}, UserRegistrationRequest >, 
-        res: Response< ApiResponse <UserRegistrationResponse> >
+        res: Response< ApiResponse <UserResponse> >
     ){
         const { name, email, password } = req.body;
-        
+        if(!name || !email || !password){
+            return res.status(400).send({
+                success: true,
+                message: 'Bad request at fields sended'
+            });
+        }
+
+        try{
+            // bcrypt - hash
+            let hash = bcrypt.hashSync(password, 10);
+
+            // profile save
+            const newUser = await models.User.create({ name, email, password: hash });
+            if(!newUser){
+                return res.status(500).send({
+                    success: false,
+                    message: 'Error at user creation at database'
+                });
+            }
+
+            console.log('_____________________');
+            console.log('User creation success');
+            console.log('_____________________');
+            return res.status(201).send({
+                success: true,
+                message: 'User creation success',
+                data: { id: newUser.id, name: newUser.name, email: newUser.email }
+            });
+        }
+        catch(error: unknown){
+            console.error('Internal server error at User Register', error);
+            return res.status(500).send({
+                success: false,
+                message: 'Internal server error at User Register',
+                errorMessage: this.getErrorMessage(error)
+            });
+        }
     };
 
 };
