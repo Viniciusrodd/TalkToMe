23db616196263e29c43e79e049c5bcc298448262
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-08-01 14:25:33 -0300

    backend - register user route + @types + verify_token middleware

---
+++ backend/src/middlewares/VerifyToken.ts
@@ -0,0 +1,36 @@
+// libs
+import * as jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { JwtPayload, VerifyErrors } from 'jsonwebtoken';
+
+// express
+import { Request, Response, NextFunction } from 'express';
+
+// dotenv
+dotenv.config();
+const secretToken = process.env.SECRET_TOKEN;
+if (!secretToken){ // force verification
+  throw new Error('SECRET_TOKEN not defined in .env');
+}
+
+// middleware
+const verifyToken = (req: Request, res: Response, next: NextFunction): void | Response =>{
+    const token = req.cookies?.token || (req.headers['authorization']?.split(' ')[1]);
+    if(!token){
+        return res.status(401).json({
+            errorVerify: 'User not authenticated'
+        });
+    }
+
+    // verify
+    jwt.verify(token, secretToken, (err: VerifyErrors | null, data: string | JwtPayload | undefined) =>{
+        if(err){
+            console.log('JWT verification failed: ', err);
+            return res.status(403).json({ invalidToken: 'Invalid token' });
+        }
+
+        // add user data at request
+        req.user = data;
+        next();
+    });
+};
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,7 +2,15 @@
 import express from 'express';
 const router = express.Router();
 
-// backend port: 2140
+// controllers
+import UserController from '../controller/UserController';
+
+// handle validation - middleware
+import validate from '../middlewares/HandleValidation';
+
+
+// user - backend port: 2140
+router.post('/register', validate, UserController.registerUser);
 
 
 export default router; 
