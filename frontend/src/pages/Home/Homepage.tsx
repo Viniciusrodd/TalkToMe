d4cc2edb97173077f543f5a91d25f680e4d0c6b8
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-08-07 12:05:44 -0300

    frontend - homepage styled

--- frontend/src/pages/Home/Homepage.tsx
+++ frontend/src/pages/Home/Homepage.tsx
@@ -6,22 +6,59 @@ import styles from './Homepage.module.css';
 import NavBar from '../../components/NavBar/NavBar';
 import SideBar from '../../components/SideBar/SideBar';
 
+// hooks
+import React, { useState, useEffect } from 'react';
+
 
 // homepage
 const Homepage = () => {
+    // states
+    const [ isTyping, setIsTyping ] = useState<string>('');
+    const [ showSendIcon, setShowSendIcon ] = useState<boolean>(false);
+
+
+    // functions
+
+    // send message icone
+    useEffect(() =>{
+        setShowSendIcon(isTyping.trim() !== '');
+    }, [isTyping]);
+
+
+    // jsx
+
+
     return (
         <div className="general_container">
             { /* navbar */ }
             <NavBar />
 
-            { /* homepage */ }
+            { /* homepage + sidebar container */ }
             <div className={ styles.home_container }>
                 { /* sidebar */ }
                 <SideBar />
 
-                <h1 className={ styles.h1_test }>
-                    homepage
-                </h1>                
+                { /* homepage */ }
+                <div className={ styles.homepage }>
+                    <h1>What can I help with ?</h1>
+                    <div className={ styles.interect_container }>
+                        <div className={ styles.utils }>
+                            <span className="material-symbols-outlined">add</span>
+                        </div>
+
+                        <input type="text" name="question" placeholder='Ask anithing...' value={ isTyping } 
+                        onChange={ (e: React.ChangeEvent<HTMLInputElement>) => setIsTyping(e.target.value) }/>
+
+
+                        <div className={ styles.utils }>
+                            { showSendIcon ? (
+                                <span className={`material-symbols-outlined ${styles.is_send}`}>send</span>
+                            ) : (
+                                <span className="material-symbols-outlined">mic</span>
+                            ) }
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
